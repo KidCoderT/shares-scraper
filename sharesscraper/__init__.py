8543dca8a03c241f4b44b0c965dbed4a5dbf9fa7
KidCoderT <Tejas75O25@gmail.com>
2020-07-05 10:39:28 +0530

    Created All the home page functionality

--- sharesscraper/__init__.py
+++ sharesscraper/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.update(
+    SECRET_KEY=b"asfsdft76rtdjghfy4uiut?/dl][fhfvnu6rgh"
+)
 
 from sharesscraper import views
--- sharesscraper/views.py
+++ sharesscraper/views.py
@@ -1,9 +1,26 @@
-from flask import render_template
+from flask import render_template, request, flash
 from sharesscraper import app
 from sharesscraper.data import my_table_data
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    if request.method == "POST":
+        search_object = request.form.get('searchObject')
+        if search_object == '':
+            flash("Please enter name into the search bar to search for the person", "warning")
+            return render_template('home.html', table_data=my_table_data, number=len(my_table_data))
+        else:
+            edited_table = []
+            for foo in range(0, len(my_table_data)):
+                if search_object in my_table_data[foo][0]:
+                    edited_table.append(my_table_data[foo])
+
+            if len(edited_table) == 0:
+                flash("Sorry we could't find anyone with the name " + search_object + ".", "error")
+                return render_template('home.html', table_data=edited_table, number=len(edited_table))
+
+            return render_template('home.html', table_data=edited_table, number=len(edited_table))
+
     return render_template('home.html', table_data=my_table_data, number=len(my_table_data))
 
