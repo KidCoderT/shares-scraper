027ebf0bb694aa213b25d478d2f38005111e7999
KidCoderT <Tejas75O25@gmail.com>
2020-07-05 08:08:28 +0530

    Got the basic data and rendered it on to the home page

--- sharesscraper/data.py
+++ sharesscraper/data.py
@@ -1,2 +1,21 @@
+import lxml
+import requests
+from bs4 import BeautifulSoup
 
-hello = "dsfsdfsdfsdf"
+my_table_data = []
+base_url = "https://trendlyne.com/portfolio/superstar-shareholders/index/"
+page = requests.get(base_url)
+soup = BeautifulSoup(page.content, 'lxml')
+page_table = soup.find("div", class_="content-wrapper").find("div", class_="tl_carousel").find("div",
+                                                                                               class_="scrolling-wrapper").find(
+    "div", class_="row").find("div", class_="card")
+page_rows = page_table.find_all("div", class_="dbdr")
+
+for div in page_rows:
+    my_table_data.append((
+        div.find("div", class_="col-lg-5").find("a").text,  # The Name
+        div.find_all("div", class_="col-lg-3")[1].text,  # The Net Worth
+        div.find_all("div", class_="col-lg-3")[0].text,  # The Companies
+        "https://trendlyne.com" + str(div.find("div", class_="col-lg-5").find("a").get("href"))  # The detailed url
+    ))
+
--- sharesscraper/views.py
+++ sharesscraper/views.py
@@ -1,9 +1,9 @@
 from flask import render_template
 from sharesscraper import app
-from sharesscraper.data import *
+from sharesscraper.data import my_table_data
 
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', table_data=my_table_data, number=len(my_table_data))
 
